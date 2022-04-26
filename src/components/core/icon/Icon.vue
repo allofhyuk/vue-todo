3b36949c2ef857359ec5f7528fc615d781d22da5
allofhyuk <gggg21367@gmail.com>
2022-04-26 13:53:15 +0900

    add Icon component for keeping origin inline svg

---
+++ src/components/core/icon/Icon.vue
@@ -0,0 +1,26 @@
+<script>
+  import DoneIcon from '@/assets/icon/inlineSvg/icon-check-small-line.svg';
+  import EditIcon from '@/assets/icon/inlineSvg/icon-pencil-small-line.svg';
+  import DeleteIcon from '@/assets/icon/inlineSvg/icon-bin-small-line.svg';
+
+  export const IconName = [
+    'DoneIcon',
+    'EditIcon',
+    'DeleteIcon',
+  ]
+
+  export default {
+    name: 'Icon',
+    props: {
+      name: {
+        type: String,
+        default: null,        
+      }
+    },
+    components: {
+      DoneIcon,
+      EditIcon,
+      DeleteIcon,
+    }
+  }
+</script>
